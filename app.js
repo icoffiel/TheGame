5053d6565ace465606017a9360d1e89a5fdafbc7
Iain <icoffiel@nerdery.com>
2016-07-04 21:35:43 -0500

    Addition of PouchDB support to save items. Items are only saved and not used at this point

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
 var rp = require('request-promise');
 var util = require('util');
+var PouchDB = require('pouchdb');
 
 var ATTACK_ITEMS = [
 
 ];
 
-var points = {
-  method: 'POST',
-  uri: 'http://thegame.nerderylabs.com/points',
-  headers: {
-    'apikey': '82af0283-6a48-4398-82c0-4cbc0217dc66'
-  },
-  json: true
-};
+console.log('Starting DB');
+var db = new PouchDB('items', { db: require('sqldown') });
 
 console.log('Starting Loop');
-setTimeout(getPoints, 2000);
+getPoints();
 
 function getPoints() {
   console.log('Points!');
+
+  var points = {
+    method: 'POST',
+    uri: 'http://thegame.nerderylabs.com/points',
+    headers: {
+      'apikey': '82af0283-6a48-4398-82c0-4cbc0217dc66'
+    },
+    json: true
+  };
+
   rp(points)
     .then(function(parsedBody) {
       console.log('Call Successful');
       console.log(util.inspect(parsedBody, false, null));
       if(parsedBody.Item !== null){
         console.log('Got Item!! ' + parsedBody.Item.Fields[0].Id);
-
-        setTimeout(function() {
-          useItem(parsedBody.Item.Fields[0].Id, parsedBody.Item.Fields[0].Name);
-        }, 2000);
+        db.post(parsedBody.Item.Fields[0])
+          .then(function(response) {
+            console.log(response);
+            setTimeout(getPoints, 2000);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+        // setTimeout(function() {
+        //   useItem(parsedBody.Item.Fields[0].Id, parsedBody.Item.Fields[0].Name);
+        // }, 2000);
       } else {
         setTimeout(getPoints, 2000);
       }
